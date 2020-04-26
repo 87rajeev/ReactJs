2ad495860e8c645ba6d83db64db2dc615b38fdf5
unknown <87.rajeev@gmail.com>
2020-04-27 03:39:56 +0530

    Created react app and sample components

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import { CardList } from './components/card-list/card-list.component';
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(Response => Response.json())
+      .then(users => this.setState({ monsters: users }));
+
+  };
+
+  render() {
+    console.log(this.state.monsters)
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
